96a88bdf96b626ef950b4b785e526dfd4afb5e9e
thenorden <thenordendev@gmail.com>
2022-04-13 01:12:19 +0300

    lesson 9

--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,9 @@
+from django.shortcuts import redirect
+from django.views import View
 from django.views.generic import ListView, DetailView
 
 from .models import Movie
+from .forms import ReviewForm
 
 
 class MoviesView(ListView):
@@ -13,4 +16,15 @@ class MovieDetailView(DetailView):
     """Полное описание фильма"""
     model = Movie
     slug_field = 'url'
-    
+
+
+class AddReview(View):
+    """Отзывы"""
+    def post(self, request, pk):
+        form = ReviewForm(request.POST)
+        movie = Movie.objects.filter(id=pk)
+        if form.is_valid():
+            form = form.save(commit=False)
+            form.movie = movie
+            form.save()
+        return redirect('/')
