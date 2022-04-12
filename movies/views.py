72270bfa7eeb636b1cc905a90c14bb9a0a8215b9
thenorden <thenordendev@gmail.com>
2022-04-13 00:07:59 +0300

    initial

---
+++ movies/views.py
@@ -0,0 +1,16 @@
+from django.views.generic import ListView, DetailView
+
+from .models import Movie
+
+
+class MoviesView(ListView):
+    """Список фильмов"""
+    model = Movie
+    queryset = Movie.objects.filter(draft=False)
+
+
+class MovieDetailView(DetailView):
+    """Полное описание фильма"""
+    model = Movie
+    slug_field = 'url'
+    
