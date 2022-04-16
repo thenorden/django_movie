3504d018d8d86e7bd71780817237dc77b8e066f5
thenorden <thenordendev@gmail.com>
2022-04-16 15:49:07 +0300

    lesson 19

--- movies/views.py
+++ movies/views.py
@@ -1,10 +1,11 @@
 from django.db.models import Q
+from django.http import HttpResponse
 from django.shortcuts import redirect
 from django.views import View
 from django.views.generic import ListView, DetailView
 
-from .models import Movie, Category, Actor, Genre
-from .forms import ReviewForm
+from .models import Movie, Category, Actor, Genre, Rating
+from .forms import ReviewForm, RatingForm
 
 
 class GenreYear:
@@ -25,6 +26,11 @@ class MovieDetailView(GenreYear, DetailView):
     """Полное описание фильма"""
     model = Movie
     slug_field = 'url'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['star_form'] = RatingForm()
+        return context
 
 
 class AddReview(View):
@@ -55,3 +61,25 @@ class FilterMoviesListView(GenreYear, ListView):
             Q(genres__in=self.request.GET.getlist('genre'))
         )
         return queryset
+
+
+class AddStarRating(View):
+    def get_client_ip(self, request):
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0]
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+        return ip
+
+    def post(self, request):
+        form = RatingForm(request.POST)
+        if form.is_valid():
+            Rating.objects.update_or_create(
+                ip=self.get_client_ip(request),
+                movie_id=int(request.POST.get('movie')),
+                defaults={'star_id': int(request.POST.get('star'))}
+            )
+            return HttpResponse(status=201)
+        else:
+            return HttpResponse(status=400)
