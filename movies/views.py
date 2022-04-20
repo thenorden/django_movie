cd8d6c9e76c24139c0f73ff4172873d2ba771f23
thenorden <thenordendev@gmail.com>
2022-04-20 10:55:25 +0300

    lesson 20

--- movies/views.py
+++ movies/views.py
@@ -20,6 +20,7 @@ class MoviesView(GenreYear, ListView):
     """Список фильмов"""
     model = Movie
     queryset = Movie.objects.filter(draft=False)
+    paginate_by = 1
 
 
 class MovieDetailView(GenreYear, DetailView):
@@ -54,13 +55,20 @@ class ActorView(GenreYear, DetailView):
 
 
 class FilterMoviesListView(GenreYear, ListView):
+    paginate_by = 1
 
     def get_queryset(self):
         queryset = Movie.objects.filter(
             Q(year__in=self.request.GET.getlist('year')) |
             Q(genres__in=self.request.GET.getlist('genre'))
-        )
+        ).distinct()
         return queryset
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['year'] = ''.join([f'year={x}&' for x in self.request.GET.getlist('year')])
+        context['genre'] = ''.join([f'genre={x}&' for x in self.request.GET.getlist('genre')])
+        return context
 
 
 class AddStarRating(View):
